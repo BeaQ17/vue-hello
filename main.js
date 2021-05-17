cc4c11f057184e856e869ca98d2a836a4f38e9bc
BeaQ17 <beaq235@gmail.com>
2021-05-17 16:44:47 +0200

    bonus done

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ const title = new Vue (
 
 
 //Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+const image = new Vue (
+    {
+        el: "#app",
+        data:{
+            helloImg: "https://images.twinkl.co.uk/tw1n/image/private/t_630/image_repo/93/1c/t2-g-100--new-mixed-language-hello-speech-bubble-display-signs-_ver_2.jpg"
+        }
+    }
+);
