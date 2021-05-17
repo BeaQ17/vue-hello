92711b24a420661f246498488df8e0ba11f97637
BeaQ17 <beaq235@gmail.com>
2021-05-17 16:07:46 +0200

    initial commit + es done

---
+++ main.js
@@ -0,0 +1,14 @@
+//vue-hello: 
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const title = new Vue (
+    {
+        el: "#root",
+        data:{
+            msg: "Hello VueJS"
+        }
+    }
+);
+
+
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
